11abe347554d15743efa53d074b9768bcafd5ac7
xuanlubin <xlb900907>
2017-03-24 16:38:21 +0800

    账号失效处理

---
+++ src/main/java/me/robin/wx/servlet/AddBizServlet.java
@@ -0,0 +1,26 @@
+package me.robin.wx.servlet;
+
+import me.robin.wx.service.BizQueueManager;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by xuanlubin on 2017/3/24.
+ * 添加公众号Biz
+ */
+@WebServlet(name = "AddBizServlet", value = "/add_biz")
+public class AddBizServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String biz = req.getParameter("biz");
+        if (StringUtils.isNotBlank(biz)) {
+            BizQueueManager.INS.offerNewTask(biz);
+        }
+    }
+}
