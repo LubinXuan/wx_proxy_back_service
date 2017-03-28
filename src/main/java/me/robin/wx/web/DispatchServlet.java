8b1b6afa663963db5f05234e75175f09e1d6313e
xuanlubin <xlb900907>
2017-03-28 17:49:59 +0800

    spring dispatch servlet

--- src/main/java/me/robin/wx/web/DispatchServlet.java
+++ src/main/java/me/robin/wx/web/DispatchServlet.java
@@ -7,10 +7,14 @@ import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by xuanlubin on 2017/3/28.
@@ -19,6 +23,8 @@ import java.io.IOException;
 public class DispatchServlet extends HttpServlet {
 
     private AbstractApplicationContext wac;
+
+    private Map<String, Boolean> init = new ConcurrentHashMap<>();
 
     @Override
     public void init() throws ServletException {
@@ -31,6 +37,38 @@ public class DispatchServlet extends HttpServlet {
         String path = ((HttpServletRequest) req).getRequestURI();
         String servletPath = StringUtils.substringAfter(path, ((HttpServletRequest) req).getContextPath());
         Servlet servlet = this.wac.getBean(servletPath, Servlet.class);
+        if (!init.containsKey(path)) {
+            WebInitParam[] initParams = servlet.getClass().getAnnotationsByType(WebInitParam.class);
+            ConcurrentHashMap<String, String> parameterMap = new ConcurrentHashMap<>();
+            if (null != initParams && initParams.length > 0) {
+                for (WebInitParam initParam : initParams) {
+                    parameterMap.put(initParam.name(), initParam.value());
+                }
+            }
+            ServletConfig servletConfig = new ServletConfig() {
+                @Override
+                public String getServletName() {
+                    return servlet.getClass().getSimpleName();
+                }
+
+                @Override
+                public ServletContext getServletContext() {
+                    return getServletConfig().getServletContext();
+                }
+
+                @Override
+                public String getInitParameter(String name) {
+                    return parameterMap.get(name);
+                }
+
+                @Override
+                public Enumeration<String> getInitParameterNames() {
+                    return parameterMap.keys();
+                }
+            };
+            servlet.init(servletConfig);
+            init.put(path, true);
+        }
         servlet.service(req, res);
     }
 
